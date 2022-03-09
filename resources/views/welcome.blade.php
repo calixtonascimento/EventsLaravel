614210015ed917d7d52a200412c7b559418f1785
Calixto <calixtoalbea@gmail.com>
2022-03-08 22:43:49 -0300

    Add: Migrations e Estilizações

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,28 @@
 
 @section('content')
 
-<h1>Teste</h1>
-<img src="/img/banner.jpg" alt="" srcset="">
-@if(10 < 5)
-    <p>Condição é true</p>
-@endif
-
-<p>{{$nome}}</p>
-
-@if($nome == "Luis")
-<p>O nome é Pedro</p>
-@else
-<p>O nome é {{ $nome }}</p>
-@endif
-
-@for($i = 0; $i < count($arr); $i++)
-    <p>{{ $arr[$i] }} - {{ $i }}</p>
-    @if($i == 2)
-        <p>o i é 2</p>
-    @endif
-@endfor
-
-<!-- Comentário HTML -->
+<div id="search-container" class="col-md-12">
+    <h1>Busque um Evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2 class="subtitle">Próximos Eventos</h2>
+    <p>Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                <div class="card-body">
+                    <p class="card-date">08/03/2022</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">X Participantes</p>
+                    <a href="#" class="btn btn-primary">Saber Mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\Event;
+use App\Http\Controllers\EventController;
 
-Route::get('/', [Event::class, 'index']);
-Route::get('/events/create', [Event::class, 'create']);
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::get('/produtos', function() {
-    
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/products', function () {
-    return view('products');
 });
